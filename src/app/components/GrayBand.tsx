45456876484b918c7b2f0280aae4b42d041b3f88
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2023-10-30 15:38:04 +0100

    gray band done

--- src/app/components/GrayBand.tsx
+++ src/app/components/GrayBand.tsx
@@ -17,15 +17,15 @@ type GrayBandProps = {
 
 const GrayBand = ({ itemOne, itemTwo }: GrayBandProps) => {
   return (
-    <section className="gray-bg flex flex-col sm:flex-row py-6 text-gray-300">
-      <div className="sm:basis-1/2 sm:border-r-2 border-gray-300 flex justify-center items-center hover:text-gray-50">
-        <Link href={itemOne.url} className="flex flex-row sm:flex-col gap-2 justify-center items-center">
+    <section className="gray-bg flex flex-col sm:flex-row py-6 text-gray-300 gap-4">
+      <div className="sm:basis-1/2 border-b-2 sm:border-b-0 sm:border-r-2 border-gray-400 flex justify-center items-center hover:text-gray-50 pb-4 sm:pb-0">
+        <Link href={itemOne.url} className="flex flex-row  gap-2 justify-center items-center">
           <Image src={itemOne.icon} alt={"Icon"} width={28} height={28} />
           {itemOne.text}
         </Link>
       </div>
       <div className="sm:basis-1/2 flex justify-center items-center hover:text-gray-50">
-        <Link href={itemTwo.url} className="flex flex-row lg:flex-col gap-2 justify-center items-center">
+        <Link href={itemTwo.url} className="flex flex-row gap-2 justify-center items-center">
           <Image src={itemTwo.icon} alt={"Icon"} width={28} height={28} />
           {itemTwo.text}
         </Link>
