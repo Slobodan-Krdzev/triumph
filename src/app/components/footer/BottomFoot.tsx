f397a891913228a1e56a0fa484be9b6245b2e99c
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2023-10-31 15:36:28 +0100

    bottom footer made separate component

---
+++ src/app/components/footer/BottomFoot.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const BottomFoot = () => {
+  return (
+    <>
+      <div className="py-4 border-b-4 border-b-zinc-900 flex items-center gap-4">
+        {/* NEMAT FB */}
+
+        <Link
+          href={"https://www.instagram.com/triumphmotorcycles_skopje/"}
+          target="_blank"
+        >
+          <Image
+            src={"instagram.svg"}
+            alt={"Instagram Icon"}
+            width={20}
+            height={20}
+          />
+        </Link>
+        <Link href={"https://twitter.com/OfficialTriumph"} target="_blank">
+          <Image
+            src={"twitter.svg"}
+            alt={"Instagram Icon"}
+            width={20}
+            height={20}
+          />
+        </Link>
+        <Link
+          href={"https://www.youtube.com/user/OfficialTriumph"}
+          target="_blank"
+        >
+          <Image
+            src={"youtube.svg"}
+            alt={"Instagram Icon"}
+            width={20}
+            height={20}
+          />
+        </Link>
+      </div>
+
+      <div className="pt-4 pb-8 flex justify-between text-neutral-500">
+        <div className="flex gap-4">
+          <Link href={"/global-content/contact-us"} className="text-sm">
+            Контакт
+          </Link>
+          <Link
+            href={"/global-content/terms-and-conditions"}
+            className="text-sm"
+          >
+            Легалност
+          </Link>
+        </div>
+        <p className="text-sm">Ⓒ 2023 Thriumph Motorcycles</p>
+      </div>
+    </>
+  );
+};
+
+export default BottomFoot;
--- src/app/components/footer/DesktopFooter.tsx
+++ src/app/components/footer/DesktopFooter.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import FooterList from "./DescktopFooterList";
 import Image from "next/image";
+import BottomFoot from "./BottomFoot";
 
 const footerLists = [
   {
@@ -103,48 +104,7 @@ const DesktopFooter = () => {
           <FooterList key={list.title} title={list.title} items={list.items} />
         ))}
       </div>
-      <div className="py-4 border-b-4 border-b-zinc-900 flex items-center gap-4">
-        {/* NEMAT FB */}
-        
-        <Link
-          href={"https://www.instagram.com/triumphmotorcycles_skopje/"}
-          target="_blank"
-        >
-          <Image
-            src={"instagram.svg"}
-            alt={"Instagram Icon"}
-            width={20}
-            height={20}
-          />
-        </Link>
-        <Link href={"https://twitter.com/OfficialTriumph"} target="_blank">
-          <Image
-            src={"twitter.svg"}
-            alt={"Instagram Icon"}
-            width={20}
-            height={20}
-          />
-        </Link>
-        <Link
-          href={"https://www.youtube.com/user/OfficialTriumph"}
-          target="_blank"
-        >
-          <Image
-            src={"youtube.svg"}
-            alt={"Instagram Icon"}
-            width={20}
-            height={20}
-          />
-        </Link>
-      </div>
-
-      <div className="pt-4 pb-8 flex justify-between text-neutral-500">
-        <div className="flex gap-4">
-          <Link href={'/global-content/contact-us'} className="text-sm">Контакт</Link>
-          <Link href={'/global-content/terms-and-conditions'} className="text-sm">Легалност</Link>
-        </div>
-        <p className="text-sm">Ⓒ 2023 Thriumph Motorcycles</p>
-      </div>
+      <BottomFoot/>
     </footer>
   );
 };
