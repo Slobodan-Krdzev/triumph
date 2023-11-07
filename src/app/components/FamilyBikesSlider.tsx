8427b44a5cdbba740eca4805a18c5b855428c77c
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2023-11-07 20:40:07 +0100

    card component created for better usability

--- src/app/components/FamilyBikesSlider.tsx
+++ src/app/components/FamilyBikesSlider.tsx
@@ -1,75 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
+import Card from "./Card";
 
 type FamilyBikesSliderProps = {
   itemsToRender: any[];
 };
 
 const FamilyBikesSlider = ({ itemsToRender = [] }: FamilyBikesSliderProps) => {
+
+
   if (itemsToRender && itemsToRender.length >= 3) {
     return (
       <div className="flex justify-start gap-8 overflow-x-scroll bg-white rounded-sm slight-white-bg ">
         {itemsToRender.map((item: any) => (
-          <div
-            key={item.id}
-            className="bg-white shadow-xl lg:mb-8 flex flex-col justify-between rounded-md"
-          >
-            <div className="">
-              <Image
-                src={item.gallery.modelImage.src}
-                alt={item.gallery.modelImage.alt}
-                width={271}
-                height={150}
-              />
-            </div>
-
-            <div className="mb-8 p-4 text-center flex flex-col justify-start w-72 grow">
-              <h3 className="text-xl font-semibold uppercase tracking-tighter">
-                {item.title}
-              </h3>
-              <p className="text-md mb-4">
-                {item.price === "Наскоро"
-                  ? "Цени Наскоро."
-                  : `Oд ${item.price} €.`}
-              </p>
-
-              {item.shortSpecInfo && (
-                <ul className="flex flex-col list-disc text-sm px-10 text-start">
-                  {item.shortSpecInfo.info1 && (
-                    <li>{item.shortSpecInfo.info1}</li>
-                  )}
-                  {item.shortSpecInfo.info2 && (
-                    <li>{item.shortSpecInfo.info2}</li>
-                  )}
-                  {item.shortSpecInfo.info3 && (
-                    <li>{item.shortSpecInfo.info3}</li>
-                  )}
-                </ul>
-              )}
-            </div>
-            <div className="flex border-t-2 border-gray-300 py-2 items-center text-center ">
-              <Link
-                href={`${
-                  item.model
-                    ? `/configure/bike/${item.model}`
-                    : `/configure/bikes/${item.subFamilyName}`
-                }`}
-                className="text-sm text-red uppercase w-1/2"
-              >
-                Конфигурација
-              </Link>
-              <Link
-                href={`${
-                  item.model
-                    ? `/motorcycles/${item.category}/${item.subFamilyCategory}/${item.model}`
-                    : `${item.url}`
-                }`}
-                className="text-md p-2 w-1/2 uppercase text-sm border-l-2 border-gray-300"
-              >
-                Види Детали
-              </Link>
-            </div>
-          </div>
+          <Card key={item.id} item={item} />
         ))}
       </div>
     );
@@ -79,66 +23,7 @@ const FamilyBikesSlider = ({ itemsToRender = [] }: FamilyBikesSliderProps) => {
     return (
       <div className="flex justify-start gap-8">
         {itemsToRender.map((item: any) => (
-          <div
-            key={item.id}
-            className="bg-white shadow-lg lg:mb-8 flex flex-col justify-between rounded-md"
-          >
-            <div className="basis-2/6">
-              <Image
-                src={item.gallery.modelImage.src}
-                alt={item.gallery.modelImage.alt}
-                width={271}
-                height={150}
-              />
-            </div>
-
-            <div className="mb-8 p-4 text-center flex flex-col justify-start basis-2/6 w-72">
-              <h3 className="text-xl font-semibold uppercase tracking-tighter">
-                {item.title}
-              </h3>
-              <p className="text-md mb-4">
-                {item.price === "Наскоро"
-                  ? "Цени Наскоро."
-                  : `Oд ${item.price} €.`}
-              </p>
-
-              {item.shortSpecInfo && (
-                <ul className="flex flex-col list-disc text-sm px-10 text-start">
-                  {item.shortSpecInfo.info1 && (
-                    <li>{item.shortSpecInfo.info1}</li>
-                  )}
-                  {item.shortSpecInfo.info2 && (
-                    <li>{item.shortSpecInfo.info2}</li>
-                  )}
-                  {item.shortSpecInfo.info3 && (
-                    <li>{item.shortSpecInfo.info3}</li>
-                  )}
-                </ul>
-              )}
-            </div>
-            <div className="flex border-t-2 border-gray-300 py-2 items-center text-center ">
-              <Link
-                href={`${
-                  item.model
-                    ? `/configure/bike/${item.model}`
-                    : `/configure/bikes/${item.subFamilyName}`
-                }`}
-                className="text-sm text-red uppercase w-1/2"
-              >
-                Конфигурација
-              </Link>
-              <Link
-                href={`${
-                  item.model
-                    ? `/motorcycles/${item.category}/${item.subFamilyCategory}/${item.model}`
-                    : `${item.url}`
-                }`}
-                className="text-md p-2 w-1/2 uppercase text-sm border-l-2 border-gray-300"
-              >
-                Види Детали
-              </Link>
-            </div>
-          </div>
+          <Card key={item.id} item={item} />
         ))}
       </div>
     );
--- src/app/configure/bikes/[typeOfFamily]/page.tsx
+++ src/app/configure/bikes/[typeOfFamily]/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const ConfigTypeOfFamPage = ({params}: any) => {
   return (
-    <div>{params.typeOfFamily}</div>
+    <div>{params.typeOfFamily}
+        ODVE TREBA SITE BIKES OD TOJ SUBFAMILY
+    </div>
   )
 }
 
