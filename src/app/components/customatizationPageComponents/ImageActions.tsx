b2a29d8066baf2abb21c7d1c03f975ace4269f69
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2023-11-10 01:57:54 +0100

    reversal done

--- src/app/components/customatizationPageComponents/ImageActions.tsx
+++ src/app/components/customatizationPageComponents/ImageActions.tsx
@@ -1,31 +1,33 @@
-'use client'
+"use client";
 import { faArrowsLeftRight } from "@fortawesome/free-solid-svg-icons/faArrowsLeftRight";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React, { useState } from "react";
 
 const ImageActions = () => {
+  const [isImageReversed, setIsImageReversed] = useState(false);
 
-    const [isImageReversed, setIsImageReversed] = useState(false)
+  const router = useRouter();
+  const pathname = usePathname();
+  const queries = useSearchParams().get("color");
 
-    const router = useRouter()
-    const pathname = usePathname()
-    const queries = useSearchParams().get('color')
+  const handleReversal = () => {
+    router.push(`${pathname}?color=${queries}&reversed=${isImageReversed}`);
 
-    const handleReversal = () => {
-
-            
-            router.push(`${pathname}?color=${queries}&reversed=${isImageReversed}`)
-
-            setIsImageReversed(!isImageReversed)
-            
-    }
+    setIsImageReversed(!isImageReversed);
+  };
 
   return (
     <div>
-      <button className="absolute bottom-10 left-1/2 bg p-3 rounded-full" onClick={handleReversal}>
-        <FontAwesomeIcon icon={faArrowsLeftRight} size="xl" color="white"/>
-      </button>
+      <div className="absolute group bottom-10  left-1/2">
+        <button
+          className="p-3 bg rounded-full"
+          onClick={handleReversal}
+        >
+          <FontAwesomeIcon icon={faArrowsLeftRight} size="xl" color="white" />
+        </button>
+        <p className="absolute -bottom-5 left-1 hidden group-hover:block text-xs">Сврти</p>
+      </div>
     </div>
   );
 };
