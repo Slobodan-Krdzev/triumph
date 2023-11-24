869c609aabb74b0f7b44790723d2cf37b2bafde8
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2023-11-24 13:36:09 +0100

    fix caro font size

--- src/app/components/homePageComponents/MainCarousell/MainCarousellItem.tsx
+++ src/app/components/homePageComponents/MainCarousell/MainCarousellItem.tsx
@@ -26,7 +26,7 @@ const MainCarousellItem = ({
 }: MainCarousellItemProps) => {
   return (
     <div
-      className=""
+      className="relative"
       style={{
         minWidth: "100%",
         backgroundImage: `url('${image}')`,
@@ -36,53 +36,54 @@ const MainCarousellItem = ({
         height: "auto",
       }}
     >
-      {video && (
-        <div className="h-full relative" 
-        style={{width:'100vw'}}>
-          <video
-          className="relative object-fill"
-            autoPlay
-            muted
-            loop
-            
-            style={{
-              height: "auto",
-              minWidth: "92%"
-            }}
-          >
-            <source
-            className="absolute w-full h-full top-0 left-0"
-              src={video}
-              type="video/webm"
-            />
-            
-          </video>
+      <div className="h-full " style={{ width: "100vw" }}>
+        {video && (
+          <>
+            <video
+              className="relative object-fill"
+              autoPlay
+              muted
+              loop
+              style={{
+                height: "auto",
+                minWidth: "92%",
+              }}
+            >
+              <source
+                className="absolute w-full h-full top-0 left-0"
+                src={video}
+                type="video/webm"
+              />
+            </video>
 
-          <div className="ml-20 absolute top-52">
-          <h2 className="mb-4 uppercase font-bold text-4xl text-white">
-            {title}
-          </h2>
-          <p className="mb-4 w-3/4 text-white">{desc}</p>
-          <div className="flex gap-4">
-          <MainBtn
-            text={link1.text}
-            bgBlack={false}
-            isLink={true}
-            link={link1.url}
-          />
-          {link2 && (
-            <MainBtn
-              text={link2.text}
-              bgBlack={true}
-              isLink={true}
-              link={link2.url}
-            />
-          )}
-          </div>
-          
-        </div>
-        </div>
-      )}
+            
+          </>
+        )}
+        <div className="ml-44 absolute top-64">
+              <h2 className="mb-4 uppercase font-bold text-7xl text-white w-2/4">
+                {title}
+              </h2>
+              <p className="mb-4 w-2/4 text-white text-2xl font-semibold">
+                {desc}
+              </p>
+              <div className="flex gap-4">
+                <MainBtn
+                  text={link1.text}
+                  bgBlack={false}
+                  isLink={true}
+                  link={link1.url}
+                />
+                {link2 && (
+                  <MainBtn
+                    text={link2.text}
+                    bgBlack={true}
+                    isLink={true}
+                    link={link2.url}
+                  />
+                )}
+              </div>
+            </div>
+      </div>
 
       <div className="overlay-carousell-btn w-full h-full flex justify-start pt-40 text-white">
         <div className="ml-20">
@@ -91,22 +92,21 @@ const MainCarousellItem = ({
           </h2>
           <p className="mb-4 w-3/4">{desc}</p>
           <div className="flex gap-4">
-          <MainBtn
-            text={link1.text}
-            bgBlack={false}
-            isLink={true}
-            link={link1.url}
-          />
-          {link2 && (
             <MainBtn
-              text={link2.text}
-              bgBlack={true}
+              text={link1.text}
+              bgBlack={false}
               isLink={true}
-              link={link2.url}
+              link={link1.url}
             />
-          )}
+            {link2 && (
+              <MainBtn
+                text={link2.text}
+                bgBlack={true}
+                isLink={true}
+                link={link2.url}
+              />
+            )}
           </div>
-          
         </div>
       </div>
     </div>
