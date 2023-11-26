04c4a5186d2f9f786741ecbfe78b4d6a326571e7
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2023-11-26 18:57:49 +0100

    models page done

--- src/app/motorcycles/adventure/[subFamily]/models/page.tsx
+++ src/app/motorcycles/adventure/[subFamily]/models/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import BikeListingNoSlider from '@/app/components/familiySharedComponents/BikeListingNoSlider'
+import { BIKES } from '@/app/constants/constants'
 
-const SubFamilyModelsPage = () => {
-  return (
-    <div>SubFamilyModelsPage</div>
-  )
+const SubFamilyModelsPage = async ({params}: any) => {
+
+  const model = params.subFamily
+
+  try{
+
+    const bikesRes = await fetch(`${BIKES}?subFamilyCategory=${model}`)
+    const bikesData = await bikesRes.json()
+
+    return (
+      <main className='bg-white md:py-16 py-4'>
+        <h1 className='uppercase text-4xl font-semibold text-center md:mb-16 mb-4'>{model}</h1>
+        <BikeListingNoSlider
+            bikes={bikesData}
+            configureLink={true}/>
+      </main>
+    )
+  }catch(err) {
+    console.log(err);
+    return 
+    
+  }
+
+ 
 }
 
 export default SubFamilyModelsPage
