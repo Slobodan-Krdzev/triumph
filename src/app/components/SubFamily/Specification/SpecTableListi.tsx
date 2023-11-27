0f4c1deebeccbb6b96a45139bd9183ff35ee9518
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2023-11-27 04:14:41 +0100

    stats

--- src/app/components/SubFamily/Specification/SpecTableListi.tsx
+++ src/app/components/SubFamily/Specification/SpecTableListi.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+"use client";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 type SpecTableListItemType = {
-    title: string,
-    desc: string
-}
+  title: string;
+  desc: string;
+};
 
 type SpecTableListPropsType = {
   items: SpecTableListItemType[];
@@ -11,15 +14,38 @@ type SpecTableListPropsType = {
 };
 
 const SpecTableListi = ({ items = [], title }: SpecTableListPropsType) => {
+  const [isListOpen, setisListOpen] = useState(false);
+
+  const handleList = () => {
+    setisListOpen(!isListOpen);
+  };
+
   return (
-    <div className="m-auto lg:w-8/12 md:w-9/12 w-12/12">
-        <p className="bg uppercase text-xl tracking-tighter text-white px-4 py-4 font-semibold">{title}</p>
-      <ul className="border-thin">
-        {items.map(item => <li key={item.title} className="flex border-thin-gray-bottom">
-            <p className="capitalize py-4 slight-white-bg md:text-md text-sm basis-4/12 md:basis-3/12 px-4 flex justify-start items-center text-neutral-600">{item.title}</p>
-            <p className="capitalize border-thin-left py-4 font-medium md:text-lg text-md basis-8/12 md:basis-9/12 px-4 flex justify-start items-center">{item.desc}</p>
-        </li>)}
-      </ul>
+    <div className="m-auto lg:w-8/12 md:w-9/12 w-12/12 mb-2">
+      <button
+        className="bg w-full text-left uppercase text-xl tracking-tighter text-white px-4 py-4 font-semibold flex justify-between"
+        onClick={handleList}
+      >
+        {title}
+        <FontAwesomeIcon
+          icon={isListOpen ? faChevronUp : faChevronDown}
+          color="white"
+        />
+      </button>
+      {isListOpen && (
+        <ul className="border-thin">
+          {items.map((item) => (
+            <li key={item.title} className="flex border-thin-gray-bottom">
+              <p className="capitalize py-4 slight-white-bg md:text-md text-sm basis-4/12 md:basis-3/12 px-4 flex justify-start items-center text-neutral-600">
+                {item.title}
+              </p>
+              <p className="capitalize border-thin-left py-4 font-medium md:text-lg text-md basis-8/12 md:basis-9/12 px-4 flex justify-start items-center">
+                {item.desc}
+              </p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
--- src/app/motorcycles/adventure/[subFamily]/[bike]/page.tsx
+++ src/app/motorcycles/adventure/[subFamily]/[bike]/page.tsx
@@ -3,11 +3,11 @@ import ColorNamePreviewer from "@/app/components/BikePageComponents/ColorNamePre
 import CustomizationColorsListing from "@/app/components/BikePageComponents/CustomizationColorsListing";
 import ImagePreview from "@/app/components/BikePageComponents/ImagePreview";
 import MainBtn from "@/app/components/MainBtn";
-import { BIKES } from "@/app/constants/constants";
+import SpecTableListi from "@/app/components/SubFamily/Specification/SpecTableListi";
+import SpecsTable from "@/app/components/SubFamily/Specification/SpecsTable";
+import { BIKES, FAMILIES } from "@/app/constants/constants";
 
 const BikePage = async ({ params }: any) => {
-
-
   try {
     const bikeRes = await fetch(`${BIKES}?model=${params.bike}`, {
       cache: "no-store",
@@ -15,14 +15,18 @@ const BikePage = async ({ params }: any) => {
     const bikeData = await bikeRes.json();
     const bike = bikeData[0];
 
+    const subFamRes = await fetch(`${FAMILIES}?type=adventure`);
+    const subFamData = await subFamRes.json();
+    const subFam = subFamData[0].subFamilies[bike.subFamilyCategory];
+
     return (
-      <>
-        <section className="bg-white">
+      <main className="bg-white">
+        <section >
           <div className="flex flex-col justify-end pt-8 md:pt-16 px-4 md:px-8 lg:px-16">
             <BikeTitle text={bike.title} />
             <div className="flex items-center flex-col md:flex-row gap-4 md:gap-0">
               <div className="flex flex-col w-full md:w-2/12 items-start md:justify-center lg:pl-24 md:pl-16 pl-0 order-3 md:order-1">
-                <ColorNamePreviewer bike={bike}/>
+                <ColorNamePreviewer bike={bike} />
                 <p className="text-sm font-semibold text-neutral-500">
                   Цени од:
                 </p>
@@ -54,7 +58,17 @@ const BikePage = async ({ params }: any) => {
             </div>
           </div>
         </section>
-      </>
+        <section className="py-4 md:py-1">
+          <h2 className="uppercase text-2xl md:text-5xl font-semibold text-center mb-4 md:mb-8">
+            Спецификација
+          </h2>
+          <div className="px-4">
+          <SpecTableListi items={[{title: 'Цена', desc: `€ ${bike.price}`}]} title={'Цена'}/>
+
+          </div>
+          <SpecsTable specs={subFam.subFamilyPageInfo.fullSpecs} />
+        </section>
+      </main>
     );
   } catch (err) {
     console.log(err);
