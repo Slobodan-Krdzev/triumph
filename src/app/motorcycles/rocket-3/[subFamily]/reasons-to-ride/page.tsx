7f690bb5f904bcf225ddd6cfb0786c9b591664a9
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2023-12-01 17:34:51 +0100

    rocket reasons added

---
+++ src/app/motorcycles/rocket-3/[subFamily]/reasons-to-ride/page.tsx
@@ -0,0 +1,79 @@
+import MainBtn from "@/app/components/MainBtn";
+import ReasonsListin from "@/app/components/SubFamily/Reasons/ReasonsListin";
+import SecondaryPagesHeroSection from "@/app/components/SubFamily/SecondaryPagesHeroSection";
+import YouTubePromo from "@/app/components/SubFamily/YouTubePromo";
+import BikeInfoTextImageBtn from "@/app/components/familiySharedComponents/BikeInfoTextImageBtn";
+import SectionTitleH2 from "@/app/components/familiySharedComponents/SectionTitleH2";
+import { BIKES, FAMILIES } from "@/app/constants/constants";
+import React from "react";
+
+const ReasonsRocketPage = async () => {
+  try {
+    const familyRes = await fetch(`${FAMILIES}?type=rocket-3`, {
+      cache: "no-store",
+    });
+    const familyData = await familyRes.json();
+    const subFamily = familyData[0].subFamilies["rocket-3"];
+
+    const bikesRes = await fetch(`${BIKES}?model=${"rocket-3"}`);
+    const bikesData = await bikesRes.json();
+
+    return (
+      <>
+        <SecondaryPagesHeroSection
+          bannerImage={subFamily.reasonsToDrive.banner.image}
+          subFamilyTitle={"rocket-3"}
+          text={`Причини зошто да ја одберете серијата`}
+        />
+
+        <main className="px-4">
+          <div className="md:w-10/12 lg:w-6/12 m-auto pt-8">
+            <SectionTitleH2
+              text={subFamily.reasonsToDrive.infoText.title}
+              color={"dark"}
+            />
+            <p className="font-normal md:text-lg text-md">
+              {subFamily.reasonsToDrive.infoText.desc}
+            </p>
+          </div>
+
+          <ReasonsListin reasons={subFamily.reasonsToDrive.reasons} />
+
+          <YouTubePromo
+            video={ subFamily.subFamilyPageInfo.youtubeVideo}
+          />
+
+          {bikesData.map((bike: any) => (
+            <BikeInfoTextImageBtn
+              key={bike.id}
+              title={bike.title}
+              desc={bike.subFamilyPromo.desc}
+              ctaBtn={{
+                text: "КОнфигурација",
+                link: `/configure/bike/${bike.model}`,
+              }}
+              image={{
+                src: bike.gallery.modelImage.src,
+                alt: bike.gallery.modelImage.alt,
+              }}
+              blackBtn={true}
+            />
+          ))}
+
+        <section className="text-center py-4 md:py-32"> 
+            <h2 className="text-4xl md:text-6xl font-bold uppercase border-b-4 border-red-600 w-1/4 m-auto pb-4 mb-4">Rocket 3</h2>
+            <p className="text-2xl md:text-4xl font-semibold uppercase mb-8">Раѓање на Нова Легенда</p>
+            <MainBtn text={"Откријте Повеќе"} bgBlack={false} isLink={true} link={'/motorcycles/rocket-3/rocket-3/models'}/>
+        </section>
+          
+        </main>
+      </>
+    );
+  } catch (err) {
+    console.log(err);
+
+    return "err";
+  }
+};
+
+export default ReasonsRocketPage;
---
+++ src/app/motorcycles/rocket-3/[subFamily]/specifications/page.tsx
@@ -0,0 +1,56 @@
+import MainBtn from "@/app/components/MainBtn";
+import SpecsTable from "@/app/components/SubFamily/Specification/SpecsTable";
+import BikeListingNoSlider from "@/app/components/familiySharedComponents/BikeListingNoSlider";
+import SectionTitleH2 from "@/app/components/familiySharedComponents/SectionTitleH2";
+import { BIKES, FAMILIES } from "@/app/constants/constants";
+
+const Rocket3SpecsPage = async () => {
+  try {
+    const subFamilyRes = await fetch(`${FAMILIES}?type=rocket-3`, {
+      cache: "no-store",
+    });
+    const subFamilyData = await subFamilyRes.json();
+    const subFamily = subFamilyData[0];
+
+    const bikesRes = await fetch(`${BIKES}?subFamilyCategory=rocket-3`, {
+      cache: "no-store",
+    });
+    const bikesData = await bikesRes.json();
+
+    return (
+      <>
+        <main className="py-4 md:py-8 lg:py-16 bg-white">
+          <h1 className="uppercase text-4xl lg:text-6xl text-black text-center font-semibold">
+            Спецификации
+          </h1>
+          <div className="flex justify-center py-4 md:py-8">
+            <MainBtn
+              text={"Види ги Моделите"}
+              bgBlack={false}
+              isLink={true}
+              link={`/motorcycles/classics/rocket-3/models`}
+            />
+          </div>
+
+          <SpecsTable
+            specs={
+              subFamily.subFamilies["rocket-3"].subFamilyPageInfo.fullSpecs
+            }
+          />
+
+          <section className="py-4 md:py-16 my-4 md:my-16 text-center border-thin-gray">
+            <SectionTitleH2 text={"Rocket-3 Модели"} color={"dark"} />
+            <BikeListingNoSlider bikes={bikesData} configureLink={true} />
+          </section>
+
+          
+        </main>
+      </>
+    );
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+};
+
+export default Rocket3SpecsPage;
