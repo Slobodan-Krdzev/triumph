c176c0c9fb56f61dbc304b87128ee56d6322f858
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2023-12-07 00:22:33 +0100

    added 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <main style={{
+        backgroundImage: 'url("/images/errorPage.avif")',
+        backgroundSize: "cover",
+        backgroundRepeat: 'repeat',
+        backgroundPosition: "center",
+        height: '100vh'
+    }}>
+
+    </main>
+  )
+}
+
+export default ErrorPage
