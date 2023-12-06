cb67385aeb4aa9b2b56fac9037d46cbb7e305404
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2023-12-06 01:09:49 +0100

    roadsters are done

--- src/app/components/SubFamily/Reasons/ReasonsListin.tsx
+++ src/app/components/SubFamily/Reasons/ReasonsListin.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 type ReasonType = {
   title: string;
   desc: string;
-  image?: string
+  image?: string;
+  video?: string;
 };
 
 type ReasonsListinProps = {
@@ -12,13 +13,15 @@ type ReasonsListinProps = {
 
 const ReasonsListin = ({ reasons = [] }: ReasonsListinProps) => {
   return (
-    <ol className="lg:w-6/12 md:w-10/12 w-11/12 m-auto md:mt-16 mt-4 list-decimal md:text-3xl text-xl font-semibold">
+    <ol className="lg:w-6/12 md:w-10/12 w-11/12 m-auto md:mt-16 mt-4 list-decimal md:text-3xl text-xl font-semibold mb-8 md:mb-16">
       {reasons.map((reason) => (
-        <li key={reason.title} className="md:mb-8 mb-4 las:mb-0">
+        <li key={reason.title} className="md:mb-8 mb-4 last:mb-0">
           <h3 className="font-medium md:text-3xl text-xl mb-2">{reason.title}</h3>
           <p className="font-normal md:text-lg text-md">{reason.desc}</p>
 
           {reason.image && <Image src={reason.image} alt={reason.title} width={1410} height={793} className="mt-2 md:mt-8"/>}
+          {reason.video && <iframe className="w-full m-auto mt-2 md:mt-8" width="1280" height="720" src={reason.video} title={reason.title}  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+           }
         </li>
       ))}
     </ol>
