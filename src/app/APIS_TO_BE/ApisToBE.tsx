46ca85fac9de1087dd572674e641b7e9317d1a8f
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2023-12-07 00:32:58 +0100

    created an apis to be folder

---
+++ src/app/APIS_TO_BE/ApisToBE.tsx
@@ -0,0 +1,28 @@
+import { LatestModelsCarousellItemType } from "../components/homePageComponents/LatestModelsCarousellSection";
+
+export const carousellItems: LatestModelsCarousellItemType[] = [
+    {
+      title: "SCRAMBLER 1200",
+      desc: "Authentic Scrambler style with class-defining adventure ready capability; the Scrambler 1200 is the real deal. The latest editions are even more accessible and ready for the everyday, weekend escapism and pure adventure.",
+      url: "/motorcycles/classic/bonneville-scrambler-1200",
+      image: "/images/home/scrambler1200LatestModelsCarousell.avif",
+    },
+    {
+      title: "TIGER 900 ARAGON EDITIONS",
+      desc: "Inspired by the dominant, race-winning performance of the Tiger 900 Rally Pro at the 2022 Baja Aragón rally.",
+      url: "/motorcycles/adventure/tiger-900",
+      image: "/images/home/tiger900AragonLatestModelsCarousell.avif",
+    },
+    {
+      title: "SPEED 400",
+      desc: "Born into a peerless bloodline, the Speed 400 has Triumph’s unique Modern Classic roadster design in its DNA.",
+      url: "/motorcycles/classic/speed-400",
+      image: "/images/home/speed400LatestModelsCarousell.avif",
+    },
+    {
+      title: "SCRAMBLER 400 X",
+      desc: "Beautifully balanced, effortlessly agile. The Scrambler 400 X is built for all riders and all terrains.",
+      url: "/motorcycles/classic/scrambler-400-x-2024",
+      image: "/images/home/scrambler400XLatestModelsCarousell.avif",
+    },
+  ];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import { carousellItems } from "./APIS_TO_BE/ApisToBE";
 import BanerAndCTASection from "./components/BanerAndCTASection";
 import GrayBand from "./components/GrayBand";
 import CardLinkItem from "./components/homePageComponents/CardLinkItem";
 import DiscoverThriumphSection from "./components/homePageComponents/DiscoverThriumphSection";
-import LatestModelsCarousellSection, { LatestModelsCarousellItemType } from "./components/homePageComponents/LatestModelsCarousellSection";
+import LatestModelsCarousellSection from "./components/homePageComponents/LatestModelsCarousellSection";
 import MainCarousell from "./components/homePageComponents/MainCarousell/MainCarousell";
 import { MAINCAROUSELLITEMS } from "./constants/constants";
 import { HomeCardLinkType } from "./types/HomeTypes/types";
@@ -28,32 +29,7 @@ const linkCardsSection: HomeCardLinkType[] = [
   },
 ];
 
-export const carousellItems: LatestModelsCarousellItemType[] = [
-  {
-    title: "SCRAMBLER 1200",
-    desc: "Authentic Scrambler style with class-defining adventure ready capability; the Scrambler 1200 is the real deal. The latest editions are even more accessible and ready for the everyday, weekend escapism and pure adventure.",
-    url: "/motorcycles/classic/bonneville-scrambler-1200",
-    image: "/images/home/scrambler1200LatestModelsCarousell.avif",
-  },
-  {
-    title: "TIGER 900 ARAGON EDITIONS",
-    desc: "Inspired by the dominant, race-winning performance of the Tiger 900 Rally Pro at the 2022 Baja Aragón rally.",
-    url: "/motorcycles/adventure/tiger-900",
-    image: "/images/home/tiger900AragonLatestModelsCarousell.avif",
-  },
-  {
-    title: "SPEED 400",
-    desc: "Born into a peerless bloodline, the Speed 400 has Triumph’s unique Modern Classic roadster design in its DNA.",
-    url: "/motorcycles/classic/speed-400",
-    image: "/images/home/speed400LatestModelsCarousell.avif",
-  },
-  {
-    title: "SCRAMBLER 400 X",
-    desc: "Beautifully balanced, effortlessly agile. The Scrambler 400 X is built for all riders and all terrains.",
-    url: "/motorcycles/classic/scrambler-400-x-2024",
-    image: "/images/home/scrambler400XLatestModelsCarousell.avif",
-  },
-];
+
 
 export default async function Home() {
 
