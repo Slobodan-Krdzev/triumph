6a4f015aa4fdd3166925c8b86803c23535f7de8d
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2023-12-07 01:43:52 +0100

    fixed endpoints

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,3 +1,3 @@
-export const BIKES = "https://ruby-hot-amethyst.glitch.me/bikes";
-export const FAMILIES = "https://sulky-leeward-scaffold.glitch.me/families";
-export const MAINCAROUSELLITEMS = "https://sulky-leeward-scaffold.glitch.me/main-carousell-items"
+export const BIKES = "https://diligent-resisted-song.glitch.me/bikes";
+export const FAMILIES = "https://diligent-resisted-song.glitch.me/families";
+export const MAINCAROUSELLITEMS = "https://diligent-resisted-song.glitch.me/main-carousell-items"
