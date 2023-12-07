bcf5554e868ccc42f4146b9124590e4f0116886e
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2023-12-07 01:50:39 +0100

    six

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,3 +1,3 @@
 export const BIKES = "https://aspiring-notch-emmental.glitch.me/bikes";
-export const FAMILIES = "https://aspiring-notch-emmental.glitch.me/bikes";
+export const FAMILIES = "https://aspiring-notch-emmental.glitch.me/families";
 export const MAINCAROUSELLITEMS = "https://aspiring-notch-emmental.glitch.me/main-carousell-items"
