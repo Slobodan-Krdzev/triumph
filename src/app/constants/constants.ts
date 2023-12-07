02a1f5eca2ab50d5a1ddc2b35210e31679760c02
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2023-12-07 01:35:55 +0100

    changed db

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,3 +1,3 @@
-export const BIKES = "https://sulky-leeward-scaffold.glitch.me/bikes";
+export const BIKES = "https://ruby-hot-amethyst.glitch.me/bikes";
 export const FAMILIES = "https://sulky-leeward-scaffold.glitch.me/families";
 export const MAINCAROUSELLITEMS = "https://sulky-leeward-scaffold.glitch.me/main-carousell-items"
