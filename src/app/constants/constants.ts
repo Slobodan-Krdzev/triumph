84e4185e19dd6ac7fc791c3cfa8a7ca4a4e4923e
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2024-01-10 22:27:44 +0100

    CONSTANTS CHANGE FOR HOST

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,4 +1,4 @@
-export const BIKES = "http://localhost:8000/bikes";
-export const FAMILIES = "http://localhost:8000/families";
-export const MAINCAROUSELLITEMS = "http://localhost:8000/main-carousell-items"
-export const LATESTCAROUSELLITEMS = "http://localhost:8000/latest-models-carousell-items"
+export const BIKES = "https://lean-decorous-libra.glitch.me/bikes";
+export const FAMILIES = "https://lean-decorous-libra.glitch.me/families";
+export const MAINCAROUSELLITEMS = "https://lean-decorous-libra.glitch.me/main-carousell-items"
+export const LATESTCAROUSELLITEMS = "https://lean-decorous-libra.glitch.me/latest-models-carousell-items"
