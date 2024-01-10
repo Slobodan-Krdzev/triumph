c9a38c204c459579f7552675e21914b6bad124b3
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2024-01-10 22:20:05 +0100

    latest carousell items API

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,3 +1,4 @@
 export const BIKES = "http://localhost:8000/bikes";
 export const FAMILIES = "http://localhost:8000/families";
 export const MAINCAROUSELLITEMS = "http://localhost:8000/main-carousell-items"
+export const LATESTCAROUSELLITEMS = "http://localhost:8000/latest-models-carousell-items"
