dc322f9924a9eb8964ee6f61ea34c5ae264c0ca9
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2024-01-12 14:37:30 +0100

    MAIN, DEVE, ADMIN IN SYNC

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,4 +1,4 @@
-export const BIKES = "https://bubbly-warm-ski.glitch.me/bikes";
-export const FAMILIES = "https://bubbly-warm-ski.glitch.me/families";
-export const MAINCAROUSELLITEMS = "https://bubbly-warm-ski.glitch.me/main-carousell-items"
-export const LATESTCAROUSELLITEMS = "https://bubbly-warm-ski.glitch.me/latest-models-carousell-items"
+export const BIKES = "http://localhost:8000/bikes";
+export const FAMILIES = "http://localhost:8000/families";
+export const MAINCAROUSELLITEMS = "http://localhost:8000/main-carousell-items"
+export const LATESTCAROUSELLITEMS = "http://localhost:8000/latest-models-carousell-items"
