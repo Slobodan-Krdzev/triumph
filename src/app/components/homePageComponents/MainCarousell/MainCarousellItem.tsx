fbb63db198a59126c0924924ed5aaf4f150d37b8
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2024-01-13 20:03:15 +0100

    fix caro

--- src/app/components/homePageComponents/MainCarousell/MainCarousellItem.tsx
+++ src/app/components/homePageComponents/MainCarousell/MainCarousellItem.tsx
@@ -60,9 +60,9 @@ const MainCarousellItem = ({
 
           <div
             className="ml-44 absolute"
-            style={{ top: "25%" }}
+            style={{ top: "15%" }}
           >
-            <h2 className="mb-4 uppercase font-bold text-7xl text-white w-2/4">
+            <h2 className="mb-4 uppercase font-bold lg:text-4xl xl:text-7xl text-white w-2/4">
               {title}
             </h2>
             <p className="mb-4 w-2/4 text-white text-2xl font-semibold">
@@ -93,12 +93,12 @@ const MainCarousellItem = ({
       {video === null && (
           <div
             className="ml-44 absolute"
-            style={{ top: "25%" }}
+            style={{ top: "15%" }}
           >
-            <h2 className="mb-4 uppercase font-bold text-7xl text-white w-2/4">
+            <h2 className="mb-4 uppercase font-bold lg:text-6xl xl:text-7xl text-white w-2/4">
               {title}
             </h2>
-            <p className="mb-4 w-2/4 text-white text-2xl font-semibold">
+            <p className="mb-4 w-2/4 text-white lg:text-xl xl:text-2xl font-semibold">
               {desc}
             </p>
             <div className="flex gap-4">
