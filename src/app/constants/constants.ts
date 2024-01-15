1107d88cdd53687fa4b7a0b680d106085be81783
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2024-01-15 14:10:31 +0100

    fix consts

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,4 +1,4 @@
-export const BIKES = "https://http://localhost:8000/bikes";
-export const FAMILIES = "https://http://localhost:8000/families";
-export const MAINCAROUSELLITEMS = "https://http://localhost:8000/main-carousell-items"
-export const LATESTCAROUSELLITEMS = "https://http://localhost:8000/latest-models-carousell-items"
+export const BIKES = "http://localhost:8000/bikes";
+export const FAMILIES = "http://localhost:8000/families";
+export const MAINCAROUSELLITEMS = "http://localhost:8000/main-carousell-items"
+export const LATESTCAROUSELLITEMS = "http://localhost:8000/latest-models-carousell-items"
