2d34bcd70b4e922d3601c39ae48fc3b854354f3b
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2024-01-17 00:08:05 +0100

    consts

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,4 +1,4 @@
-export const BIKES = "https://powerful-separate-marten.glitch.me/bikes";
-export const FAMILIES = "https://powerful-separate-marten.glitch.me/families";
-export const MAINCAROUSELLITEMS = "https://powerful-separate-marten.glitch.me/main-carousell-items"
-export const LATESTCAROUSELLITEMS = "https://powerful-separate-marten.glitch.me/latest-models-carousell-items"
+export const BIKES = "https://bronze-amused-quilt.glitch.me/bikes";
+export const FAMILIES = "https://bronze-amused-quilt.glitch.me/families";
+export const MAINCAROUSELLITEMS = "https://bronze-amused-quilt.glitch.me/main-carousell-items"
+export const LATESTCAROUSELLITEMS = "https://bronze-amused-quilt.glitch.me/latest-models-carousell-items"
