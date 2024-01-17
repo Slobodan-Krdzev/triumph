22dffdcb92cc37b41733457c3652e7824db9576d
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2024-01-17 11:16:33 +0100

    in-sync with main

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,4 +1,4 @@
-export const BIKES = "https://indigo-eager-perfume.glitch.me/bikes";
-export const FAMILIES = "https://indigo-eager-perfume.glitch.me/families";
-export const MAINCAROUSELLITEMS = "https://indigo-eager-perfume.glitch.me/main-carousell-items"
-export const LATESTCAROUSELLITEMS = "https://indigo-eager-perfume.glitch.me/latest-models-carousell-items"
+export const BIKES = "http://localhost:8000/bikes";
+export const FAMILIES = "http://localhost:8000/families";
+export const MAINCAROUSELLITEMS = "http://localhost:8000/main-carousell-items"
+export const LATESTCAROUSELLITEMS = "http://localhost:8000/latest-models-carousell-items"
