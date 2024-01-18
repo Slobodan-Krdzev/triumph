1a3764249acd5d9a40a3457e1f93f51c948b6999
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2024-01-18 14:49:01 +0100

    sync with main

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,4 +1,4 @@
-export const BIKES = "https://plucky-fantasy-song.glitch.me/bikes";
-export const FAMILIES = "https://plucky-fantasy-song.glitch.me/families";
-export const MAINCAROUSELLITEMS = "https://plucky-fantasy-song.glitch.me/main-carousell-items"
-export const LATESTCAROUSELLITEMS = "https://plucky-fantasy-song.glitch.me/latest-models-carousell-items"
+export const BIKES = "http://localhost:8000/bikes";
+export const FAMILIES = "http://localhost:8000/families";
+export const MAINCAROUSELLITEMS = "http://localhost:8000/main-carousell-items"
+export const LATESTCAROUSELLITEMS = "http://localhost:8000/latest-models-carousell-items"
