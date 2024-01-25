a5b69d96fa6785ab71e7f113170bdb3dcea0a3db
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2024-01-25 17:44:39 +0100

    new const after error

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,7 +1,7 @@
-export const BIKES = "https://dull-mirage-spy.glitch.me/bikes";
-export const FAMILIES = "https://dull-mirage-spy.glitch.me/families";
-export const SUB_FAMILIES = "https://dull-mirage-spy.glitch.me/subFamilies";
-export const MAINCAROUSELLITEMS = "https://dull-mirage-spy.glitch.me/main-carousell-items"
-export const LATESTCAROUSELLITEMS = "https://dull-mirage-spy.glitch.me/latest-models-carousell-items"
-export const PROMOS = "https://dull-mirage-spy.glitch.me/promos"
+export const BIKES = "https://gelatinous-clean-baker.glitch.me/bikes";
+export const FAMILIES = "https://gelatinous-clean-baker.glitch.me/families";
+export const SUB_FAMILIES = "https://gelatinous-clean-baker.glitch.me/subFamilies";
+export const MAINCAROUSELLITEMS = "https://gelatinous-clean-baker.glitch.me/main-carousell-items"
+export const LATESTCAROUSELLITEMS = "https://gelatinous-clean-baker.glitch.me/latest-models-carousell-items"
+export const PROMOS = "https://gelatinous-clean-baker.glitch.me/promos"
 
