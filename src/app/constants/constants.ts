3f0160ea9d87f14cddc38ec16ebb32d758b7850e
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2024-01-26 21:36:22 +0100

    sync with main

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,7 +1,7 @@
-export const BIKES = "https://gelatinous-clean-baker.glitch.me/bikes";
-export const FAMILIES = "https://gelatinous-clean-baker.glitch.me/families";
-export const SUB_FAMILIES = "https://gelatinous-clean-baker.glitch.me/subFamilies";
-export const MAINCAROUSELLITEMS = "https://gelatinous-clean-baker.glitch.me/main-carousell-items"
-export const LATESTCAROUSELLITEMS = "https://gelatinous-clean-baker.glitch.me/latest-models-carousell-items"
-export const PROMOS = "https://gelatinous-clean-baker.glitch.me/promos"
+export const BIKES = "http://localhost:8000/bikes";
+export const FAMILIES = "http://localhost:8000/families";
+export const SUB_FAMILIES = "http://localhost:8000/subFamilies";
+export const MAINCAROUSELLITEMS = "http://localhost:8000/main-carousell-items"
+export const LATESTCAROUSELLITEMS = "http://localhost:8000/latest-models-carousell-items"
+export const PROMOS = "http://localhost:8000/promos"
 
