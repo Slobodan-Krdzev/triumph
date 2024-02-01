3dfef983a28870ce1bb45d1324f2423e229f28e8
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2024-02-01 01:11:15 +0100

    newDB and subFamily finish

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,7 +1,7 @@
-export const BIKES = "http://localhost:8000/bikes";
-export const FAMILIES = "http://localhost:8000/families";
-export const SUB_FAMILIES = "http://localhost:8000/subFamilies";
-export const MAINCAROUSELLITEMS = "http://localhost:8000/main-carousell-items"
-export const LATESTCAROUSELLITEMS = "http://localhost:8000/latest-models-carousell-items"
-export const PROMOS = "http://localhost:8000/promos"
+export const BIKES = "https://youthful-alike-squirrel.glitch.me/bikes";
+export const FAMILIES = "https://youthful-alike-squirrel.glitch.me/families";
+export const SUB_FAMILIES = "https://youthful-alike-squirrel.glitch.me/subFamilies";
+export const MAINCAROUSELLITEMS = "https://youthful-alike-squirrel.glitch.me/main-carousell-items"
+export const LATESTCAROUSELLITEMS = "https://youthful-alike-squirrel.glitch.me/latest-models-carousell-items"
+export const PROMOS = "https://youthful-alike-squirrel.glitch.me/promos"
 
