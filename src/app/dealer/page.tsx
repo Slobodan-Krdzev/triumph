9a1da65b61ab06d621e2ff1e7856e454620b14fa
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2024-02-08 01:10:35 +0100

    dealer page work

--- src/app/dealer/page.tsx
+++ src/app/dealer/page.tsx
@@ -1,5 +1,6 @@
+import Image from "next/image";
+import logo1 from "../../../public/logoBlack.png";
 import Link from "next/link";
-import React from "react";
 
 const DealerPage = () => {
   return (
@@ -17,22 +18,81 @@ const DealerPage = () => {
         className="bg-white m-auto w-6/12 rounded-lg flex flex-col justify-between items-start"
         style={{ height: "60vh" }}
       >
-        <h1 className="text-center py-4 md:py-8 text-xl md:text-5xl font-bold basis-1/5 w-full">
-          Triumph Motorcycles Macedonia
-        </h1>
+        <div className="flex justify-center items-center w-full h-full basis-1/5 pt-12">
+          <Image src={logo1} alt={"Logo"} height={100} width={228} />
+        </div>
+        <div className="basis-4/5 w-full flex justify-between">
+          <div className="flex flex-col justify-center items-start basis-1/2 p-20">
+            <p className="font-semibold text-lg">Адреса:</p>
+            <p className="mb-8">Св. Кирил и Методиј бр.20, Скопје 1000</p>
 
-        <div className="basis-4/5 w-full flex justify-between ">
-          <div className="flex flex-col justify-center items-start basis-1/2">
-            <p>Sv. Kiril i Metodij 20, Skopje 1000</p>
+            <p className="font-semibold text-lg">Телефони за контакт:</p>
+            <ul className="mb-8">
+              <li>
+                <a href="tel:+389 2 312 0706" className="underline">+389 2 312 0706</a>
+              </li>
+              <li>
+                <a href="tel:+389 78 255 535" className="underline">+389 78 255 535</a>
+              </li>
+            </ul>
 
-            <p>Phone: +389 2 312 0706</p>
-            <p>Phone: +389 78 255 535</p>
-            <p><a href="mailto:info@triumphmotorcycles.mk">Email: info@triumphmotorcycles.mk</a></p>
-            <p></p>
+            <p className="font-semibold text-lg">Email:</p>
+            <a href="mailto:info@triumphmotorcycles.mk" className="underline">
+              info@triumphmotorcycles.mk
+            </a>
+
+            <div className="flex justify-between items-center gap-4 mt-4">
+            <Link
+                href={"https://www.facebook.com/triumphmotorcyclesskopje"}
+                target="_blank"
+              >
+                <Image
+                  src={"/facebook.svg"}
+                  alt={"Facebook Icon"}
+                  width={20}
+                  height={20}
+                />
+              </Link>
+              <Link
+                href={"https://www.instagram.com/triumphmotorcycles_skopje/"}
+                target="_blank"
+              >
+                <Image
+                  src={"/instagram.svg"}
+                  alt={"Instagram Icon"}
+                  width={20}
+                  height={20}
+                />
+              </Link>
+              <Link
+                href={"https://twitter.com/OfficialTriumph"}
+                target="_blank"
+              >
+                <Image
+                  src={"/twitter.svg"}
+                  alt={"Instagram Icon"}
+                  width={20}
+                  height={20}
+                />
+              </Link>
+              <Link
+                href={"https://www.youtube.com/user/OfficialTriumph"}
+                target="_blank"
+              >
+                <Image
+                  src={"/youtube.svg"}
+                  alt={"Instagram Icon"}
+                  width={20}
+                  height={20}
+                />
+              </Link>
+
+
+            </div>
           </div>
 
           <div className="flex flex-col justify-center items-center basis-1/2">
-            mapta  
+            mapta
           </div>
         </div>
       </div>
