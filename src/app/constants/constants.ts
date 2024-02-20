2d7d10b2bfe494232d5b6a28671583ba032efe03
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2024-02-20 20:13:38 +0100

    new db

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,7 +1,7 @@
-export const BIKES = "hammerhead-shade-bagpipe.glitch.me/bikes";
-export const FAMILIES = "hammerhead-shade-bagpipe.glitch.me/families";
-export const SUB_FAMILIES = "hammerhead-shade-bagpipe.glitch.me/subFamilies";
-export const MAINCAROUSELLITEMS = "hammerhead-shade-bagpipe.glitch.me/main-carousell-items"
-export const LATESTCAROUSELLITEMS = "hammerhead-shade-bagpipe.glitch.me/latest-models-carousell-items"
-export const PROMOS = "hammerhead-shade-bagpipe.glitch.me/promos"
+export const BIKES = "https://hammerhead-shade-bagpipe.glitch.me/bikes";
+export const FAMILIES = "https://hammerhead-shade-bagpipe.glitch.me/families";
+export const SUB_FAMILIES = "https://hammerhead-shade-bagpipe.glitch.me/subFamilies";
+export const MAINCAROUSELLITEMS = "https://hammerhead-shade-bagpipe.glitch.me/main-carousell-items"
+export const LATESTCAROUSELLITEMS = "https://hammerhead-shade-bagpipe.glitch.me/latest-models-carousell-items"
+export const PROMOS = "https://hammerhead-shade-bagpipe.glitch.me/promos"
 
