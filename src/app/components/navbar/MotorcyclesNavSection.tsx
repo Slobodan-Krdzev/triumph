7dbd42c93d6d377568db0cc4ea7a04f56aafe0dc
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2024-02-20 17:07:00 +0100

    fixed err

--- src/app/components/navbar/MotorcyclesNavSection.tsx
+++ src/app/components/navbar/MotorcyclesNavSection.tsx
@@ -8,11 +8,7 @@ const MotorcyclesNavSection = ({families, bikes, bikeToRender}: any) => {
 
   return (
     <>
-<<<<<<< HEAD
-      <div className="lg:px-16 2xl:px-48">
-=======
       <div className="lg:px-40">
->>>>>>> c6a467ddcd7b5ccc9d3851fe06d179a31a9f4f5c
         <ul className="flex font-bold text-md gap-6 mt-4">
           <li>
             <button className="uppercase py-3 px-4 rounded-t-md text-neutral-500 focus:text-black hover:text-black" onClick={() => setAreMotorsShown(true)}
@@ -36,11 +32,7 @@ const MotorcyclesNavSection = ({families, bikes, bikeToRender}: any) => {
         </ul>
       </div>
 
-<<<<<<< HEAD
-      <div className="bg-white lg:px-20 2xl:px-52">
-=======
       <div className="bg-white px-40">
->>>>>>> c6a467ddcd7b5ccc9d3851fe06d179a31a9f4f5c
         {areMotorsShown ? (
           <>
             <MotorcyclesNav families={families} bikes={bikes} bikeToRender={bikeToRender}/>
