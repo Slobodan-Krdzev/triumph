d0a012e28a7b0a32ce794ca5fd38e6717b01d1da
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2024-02-26 02:19:07 +0100

    fixed constants

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,7 +1,7 @@
-export const BIKES = "sugared-loud-andesaurus.glitch.me/bikes";
-export const FAMILIES = "sugared-loud-andesaurus.glitch.me/families";
-export const SUB_FAMILIES = "sugared-loud-andesaurus.glitch.me/subFamilies";
-export const MAINCAROUSELLITEMS = "sugared-loud-andesaurus.glitch.me/main-carousell-items"
-export const LATESTCAROUSELLITEMS = "sugared-loud-andesaurus.glitch.me/latest-models-carousell-items"
-export const PROMOS = "sugared-loud-andesaurus.glitch.me/promos"
+export const BIKES = "https://sugared-loud-andesaurus.glitch.me/bikes";
+export const FAMILIES = "https://sugared-loud-andesaurus.glitch.me/families";
+export const SUB_FAMILIES = "https://sugared-loud-andesaurus.glitch.me/subFamilies";
+export const MAINCAROUSELLITEMS = "https://sugared-loud-andesaurus.glitch.me/main-carousell-items"
+export const LATESTCAROUSELLITEMS = "https://sugared-loud-andesaurus.glitch.me/latest-models-carousell-items"
+export const PROMOS = "https://sugared-loud-andesaurus.glitch.me/promos"
 
