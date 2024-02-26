cf6c1614c33a65394c8679aebc49e96c941147fb
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2024-02-26 02:17:43 +0100

    NEW DB STRUCTURE / LATEST CARO FIXED / SYNC DEV

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,7 +1,7 @@
-export const BIKES = "http://localhost:8000/bikes";
-export const FAMILIES = "http://localhost:8000/families";
-export const SUB_FAMILIES = "http://localhost:8000/subFamilies";
-export const MAINCAROUSELLITEMS = "http://localhost:8000/main-carousell-items"
-export const LATESTCAROUSELLITEMS = "http://localhost:8000/latest-models-carousell-items"
-export const PROMOS = "http://localhost:8000/promos"
+export const BIKES = "sugared-loud-andesaurus.glitch.me/bikes";
+export const FAMILIES = "sugared-loud-andesaurus.glitch.me/families";
+export const SUB_FAMILIES = "sugared-loud-andesaurus.glitch.me/subFamilies";
+export const MAINCAROUSELLITEMS = "sugared-loud-andesaurus.glitch.me/main-carousell-items"
+export const LATESTCAROUSELLITEMS = "sugared-loud-andesaurus.glitch.me/latest-models-carousell-items"
+export const PROMOS = "sugared-loud-andesaurus.glitch.me/promos"
 
