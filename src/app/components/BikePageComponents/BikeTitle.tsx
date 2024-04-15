086296f6ee07bf06f6b8647f9dc7b7867b63dace
Slobodan-Krdzev <slobodankrdzev@yahoo.com>
2024-04-15 12:16:54 +0200

    fixed title bike

--- src/app/components/BikePageComponents/BikeTitle.tsx
+++ src/app/components/BikePageComponents/BikeTitle.tsx
@@ -6,7 +6,7 @@ type BikeTitleProps = {
 
 const BikeTitle = ({text}: BikeTitleProps) => {
   return (
-    <h1 className='text-4xl md:text-5xl lg:text-7xl text-neutral-200 font-bold uppercase w-full m-auto lg:w-10/12 '>{text}</h1>
+    <h1 className='text-3xl md:text-5xl lg:text-7xl text-neutral-200 font-bold uppercase w-full m-auto lg:w-10/12 '>{text}</h1>
   )
 }
 
