296e365eb1df66a9d64f9d7d0acf5c8a5cdbbb6f
Slobodan-Krdzev <slobodankrdzev@yahoo.com>
2024-06-20 18:52:55 +0200

    fixed acc page

--- src/app/components/SubFamily/ClassicAccesoryPage/AccessoryItemWrapper.tsx
+++ src/app/components/SubFamily/ClassicAccesoryPage/AccessoryItemWrapper.tsx
@@ -16,22 +16,38 @@ const AccessoryItemWrapper = ({
         <SectionTitleH2 text={title} color={"dark"} />
 
         <p className="font-normal md:text-lg text-md">{desc}</p>
-        <ul className="mt-4 md:mt-8 list-disc marker:text-red-600 marker:text-2xl list-inside">
-          {itemsList.map((item) => (
-            <li key={item} className="text-xl">
-              {item}
-            </li>
-          )) ?? ""}
-        </ul>
-      </div>
-      <div className="flex flex-col lg:flex-row justify-center gap-4">
-        {image1?.alt !== null && (
-          <Image src={image1?.src ?? ""} alt={image1?.alt ?? ""} width={image2 ? 739 : 1410 } height={image2 ? 462 : 793} />
-        )}
-        {image2?.alt !== null && (
-          <Image src={image2?.src ?? ""} alt={image2?.alt ?? ""} width={739} height={462} />
+        {itemsList[0] !== null && (
+          <ul className="mt-4 md:mt-8 list-disc marker:text-red-600 marker:text-2xl list-inside">
+            {itemsList.map((item) => (
+              <li key={item} className="text-xl">
+                {item}
+              </li>
+            )) ?? ""}
+          </ul>
         )}
       </div>
+      {image1?.alt !== null && (
+        <div className="flex flex-col lg:flex-row justify-center gap-4">
+          {image1?.alt !== null && (
+            <Image
+              src={image1?.src ?? ""}
+              alt={image1?.alt ?? ""}
+              width={image2 ? 739 : 1410}
+              height={image2 ? 462 : 793}
+              loading="lazy"
+            />
+          )}
+          {image2?.alt !== null && (
+            <Image
+              src={image2?.src ?? ""}
+              alt={image2?.alt ?? ""}
+              width={739}
+              height={462}
+              loading="lazy"
+            />
+          )}
+        </div>
+      )}
     </div>
   );
 };
