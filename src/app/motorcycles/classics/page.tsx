59335453c3c6ccf615488bb3764b261c1b5d4088
Slobodan-Krdzev <slobodankrdzev@yahoo.com>
2024-06-20 20:04:01 +0200

    classics page

--- src/app/motorcycles/classics/page.tsx
+++ src/app/motorcycles/classics/page.tsx
@@ -15,135 +15,134 @@ import { getBikesByCC } from "./helpers/getBikesByCC";
 import BikesCarousell900 from "@/app/components/classicsPageComp/BikesCarousell900";
 
 const ClassicPage = async () => {
-  try {
-    const familyRes = await fetch(`${FAMILIES}?type=classics`, {
-      next: { revalidate: 30 },
-    });
-    const familyData = await familyRes.json();
+  const familyRes = await fetch(`${FAMILIES}?type=classics`, {
+    next: { revalidate: 30 },
+  });
+  const familyData = await familyRes.json();
 
-    const bikesRes = await fetch(`${SUB_FAMILIES}?familyType=classics`, {
-      cache: "no-store",
-    });
-    const bikes = await bikesRes.json();
+  const bikesRes = await fetch(`${SUB_FAMILIES}?familyType=classics`, {
+    cache: "no-store",
+  });
+  const bikes = await bikesRes.json();
 
-    const promosRes = await fetch(`${PROMOS}?category=classics`, {
-      next: { revalidate: 30 },
-    });
-    const promos = await promosRes.json();
+  const promosRes = await fetch(`${PROMOS}?category=classics`, {
+    next: { revalidate: 30 },
+  });
+  const promos = await promosRes.json();
 
-    console.log(getBikesByCC(900, bikes));
+  return (
+    <>
+      <PageHeroSection
+        title={`${familyData[0]?.type ?? "Classics"}`}
+        video={familyData[0]?.familyPageBannerVideo ?? ""}
+      />
 
-    return (
-      <>
-        <PageHeroSection
-          title={`${familyData[0]?.type ?? "Classics"}`}
-          video={familyData[0]?.familyPageBannerVideo ?? ""}
-        />
+      <main className="bg-white">
+        <section className="pt-16 lg:pt-32">
+          <div className="flex flex-col justify-items-center items-center lg:w-2/4 w-11/12 m-auto text-center">
+            <SectionTitleH2 text="400cc engine" color="dark" />
+            <PageParagraph
+              marginBot={true}
+              text='"Speed ​​400" и "Scrambler 400 X", два сосема нови моторцикли доаѓаат во иконската палета на модерни класици на Triumph. Со карактерот и одзивот на новиот мотор од 400 кубици, прифатлива ергономија и високи спецификации, овие нови машини нудат врвни перформанси и самоуверено, агилно ракување.'
+            />
+          </div>
+        </section>
 
-        <main className="bg-white">
-          <section className="pt-16 lg:pt-32">
-            <div className="flex flex-col justify-items-center items-center lg:w-2/4 w-11/12 m-auto text-center">
-              <SectionTitleH2 text="400cc engine" color="dark" />
-              <PageParagraph
-                marginBot={true}
-                text='"Speed ​​400" и "Scrambler 400 X", два сосема нови моторцикли доаѓаат во иконската палета на модерни класици на Triumph. Со карактерот и одзивот на новиот мотор од 400 кубици, прифатлива ергономија и високи спецификации, овие нови машини нудат врвни перформанси и самоуверено, агилно ракување.'
-              />
-            </div>
-          </section>
+        {/* <section className="m-auto w-11/12 md:w-9/12">
+          {getBikesByCC(400, bikes).map((bike: any) => (
+            <BikeInfoTextImageBtn
+              key={bike.id}
+              title={bike?.title ?? "Triumph"}
+              desc={bike?.shortDesc ?? "Triumph Classics"}
+              ctaBtn={{
+                text: "Детали",
+                link: `/motorcycles/classics/${bike.subFamilyName}`,
+              }}
+              image={{
+                src:
+                  bike?.gallery?.modelImage?.src ?? "/images/triumphLogo.png",
+                alt: bike?.gallery?.modelImage?.alt ?? "Triumph Classic Bike",
+              }}
+              blackBtn={true}
+            />
+          ))}
+        </section> */}
 
-          <section className="m-auto w-11/12 md:w-9/12">
-            {/* type treba da e subFamily */}
-            {getBikesByCC(400, bikes).map((bike: any) => (
-              <BikeInfoTextImageBtn
-                key={bike.id}
-                title={bike?.title ?? "Triumph"}
-                desc={bike?.shortDesc ?? "Triumph Classics"}
-                ctaBtn={{
-                  text: "Детали",
-                  link: `/motorcycles/classics/${bike.subFamilyName}`,
-                }}
-                image={{
-                  src: bike?.gallery?.modelImage?.src ?? "/images/triumphLogo.png",
-                  alt: bike?.gallery?.modelImage?.alt ?? "Triumph Classic Bike",
-                }}
-                blackBtn={true}
-              />
-            ))}
-          </section>
+        <section className="pt-16 lg:pt-32">
+          <div className="flex flex-col justify-items-center items-center lg:w-2/4 w-11/12 m-auto text-center">
+            <SectionTitleH2 text="900cc engine" color="dark" />
+            <PageParagraph
+              marginBot={true}
+              text='"Modern Classic 900 cc" серијата на Triumph обезбедува возбудливи перформанси и управување што инспирира самодоверба.'
+            />
+          </div>
+        </section>
 
-          <section className="pt-16 lg:pt-32">
-            <div className="flex flex-col justify-items-center items-center lg:w-2/4 w-11/12 m-auto text-center">
-              <SectionTitleH2 text="900cc engine" color="dark" />
-              <PageParagraph
-                marginBot={true}
-                text='"Modern Classic 900 cc" серијата на Triumph обезбедува возбудливи перформанси и управување што инспирира самодоверба.'
-              />
-            </div>
-          </section>
+        {/* <section className="flex flex-col xl:flex-row md:px-8 px-4 md:gap-4">
+          {getBikesByCC(900, bikes).length > 3 ? (
+            <BikesCarousell900 bikes={getBikesByCC(900, bikes)} />
+          ) : (
+            <>
+              {getBikesByCC(900, bikes).map((bike: any) => (
+                <CardLinkItem
+                  key={bike.id}
+                  title={bike?.title ?? "Triumph"}
+                  image={
+                    bike?.gallery?.modelImage.src ?? "/images/triumphLogo.png"
+                  }
+                  text={"Детали"}
+                  url={`/motorcycles/classics/${bike?.subFamilyName}`}
+                  desc={bike?.shortDesc ?? ""}
+                />
+              ))}
+            </>
+          )}
+        </section> */}
 
-          <section className="flex flex-col xl:flex-row md:px-8 px-4 md:gap-4">
-            {getBikesByCC(900, bikes).length > 3 ? (
-              <BikesCarousell900 bikes={getBikesByCC(900, bikes)} />
-            ) : (
-              <>
-                {getBikesByCC(900, bikes).map((bike: any) => (
-                  <CardLinkItem
-                    key={bike.id}
-                    title={bike?.title ?? "Triumph"}
-                    image={bike?.gallery?.modelImage.src ?? "/images/triumphLogo.png"}
-                    text={"Детали"}
-                    url={`/motorcycles/classics/${bike?.subFamilyName}`}
-                    desc={bike?.shortDesc ?? ""}
-                  />
-                ))}
-              </>
-            )}
-          </section>
+        <section className="pt-16 lg:pt-32">
+          <div className="flex flex-col justify-items-center items-center lg:w-2/4 w-11/12 m-auto text-center">
+            <SectionTitleH2 text="1200cc engine" color="dark" />
+            <PageParagraph
+              marginBot={true}
+              text="Уникатни двоцилиндрични, линиски мотоцикли со мотор од 1200 кубика од Triumph имаат свој уникатен, автентичен карактер и стил."
+            />
+          </div>
+        </section>
 
-          <section className="pt-16 lg:pt-32">
-            <div className="flex flex-col justify-items-center items-center lg:w-2/4 w-11/12 m-auto text-center">
-              <SectionTitleH2 text="1200cc engine" color="dark" />
-              <PageParagraph
-                marginBot={true}
-                text="Уникатни двоцилиндрични, линиски мотоцикли со мотор од 1200 кубика од Triumph имаат свој уникатен, автентичен карактер и стил."
-              />
-            </div>
-          </section>
+        {/* <section className="flex flex-wrap px-8">
+          {getBikesByCC(1200, bikes).map((bike: any) => (
+            <DiscoverThriumphCard
+              key={bike.bikeId}
+              desc={bike?.shortDesc ?? ""}
+              image={bike?.gallery?.modelImage.src ?? "/images/triumphLogo.png"}
+              title={bike?.title ?? ""}
+              url={`/motorcycles/${bike?.familyType}/${bike?.subFamilyName}`}
+              btnText={"Детали"}
+            />
+          ))}
+        </section> */}
 
-          <section className="flex flex-wrap px-8">
-            {getBikesByCC(1200, bikes).map((bike: any) => (
-              <DiscoverThriumphCard
-                key={bike.bikeId}
-                desc={bike?.shortDesc ?? ""}
-                image={bike?.gallery?.modelImage.src ?? "/images/triumphLogo.png"}
-                title={bike?.title ?? ""}
-                url={`/motorcycles/${bike?.familyType}/${bike?.subFamilyName}`}
-                btnText={"Детали"}
-              />
-            ))}
-          </section>
+        <section className="m-auto w-11/12 md:w-10/12">
+          {promos.map((promo: PromoDataType, idx: number) => (
+            <BikeInfoTextImageBtn
+              key={promo?.title ?? idx}
+              title={promo?.title ?? "Triumph"}
+              desc={promo?.desc ?? "Triumph Classic Bike"}
+              ctaBtn={{
+                text: "Детали",
+                link: `/motorcycles/classics/${promo?.subFamilyType}`,
+              }}
+              image={{
+                src: promo?.image ?? "/images/triumphLogo.png",
+                alt: promo?.title ?? "Triumph",
+              }}
+              blackBtn={promo?.btnBlack ?? false}
+              imageOnTheLeft={idx % 2 === 0 ? true : false}
+            />
+          ))}
+        </section>
 
-          <section className="m-auto w-11/12 md:w-10/12">
-            {promos.map((promo: PromoDataType, idx: number) => (
-              <BikeInfoTextImageBtn
-                key={promo?.title ?? idx}
-                title={promo?.title ?? "Triumph"}
-                desc={promo?.desc ?? "Triumph Classic Bike"}
-                ctaBtn={{
-                  text: "Детали",
-                  link: `/motorcycles/classics/${promo?.subFamilyType}`,
-                }}
-                image={{
-                  src: promo?.image ?? "/images/triumphLogo.png",
-                  alt: promo?.title ?? 'Triumph',
-                }}
-                blackBtn={promo?.btnBlack ?? false}
-                imageOnTheLeft={idx % 2 === 0 ? true : false}
-              />
-            ))}
-          </section>
-
-          {/* <section className="lg:py-16 lg:px-8 flex flex-col ">
+        {/* <section className="lg:py-16 lg:px-8 flex flex-col ">
             <div className="text-center">
               <SectionTitleH2 text="Новата Stealth Серија" color="dark" />
             </div>
@@ -151,7 +150,7 @@ const ClassicPage = async () => {
             <StelthCarousell bikes={getBikesByEdition("stealth", bikes)} />
           </section> */}
 
-          {/* <section className="my-8">
+        {/* <section className="my-8">
             <div className="text-center lg:w-2/4 w-full px-4 lg:px-0 m-auto">
               <SectionTitleH2 text="Chrome Колекција" color="dark" />
               <PageParagraph
@@ -167,15 +166,12 @@ const ClassicPage = async () => {
             <BikeListingNoSlider bikes={getBikesByEdition("chrome", bikes)} />
           </section> */}
 
-          {familyData[0]?.grayCaro && (
-            <BottomCarousell items={familyData[0]?.grayCaro ?? []} />
-          )}
-        </main>
-      </>
-    );
-  } catch (err) {
-    return redirect("/configure");
-  }
+        {familyData[0]?.grayCaro && (
+          <BottomCarousell items={familyData[0]?.grayCaro ?? []} />
+        )}
+      </main>
+    </>
+  );
 };
 
 export default ClassicPage;
